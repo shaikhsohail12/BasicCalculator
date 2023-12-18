db2d4c240ab444f924986d070f96f9aa2c1b8f5a
shaikhsohail12 <88981555+shaikhsohail12@users.noreply.github.com>
2023-12-18 18:20:57 +0530

    basic code

---
+++ index.js
@@ -0,0 +1,12 @@
+function Enter1(value) {
+    document.getElementById('text1').value += value;
+}
+
+
+function clr() {
+    document.getElementById('text1').value = '';
+}
+
+function calcu(){
+    document.getElementById('text1').value = eval(document.getElementById('text1').value);
+}
